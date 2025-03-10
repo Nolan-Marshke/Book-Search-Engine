8ca8f629648629f188002773dcf1b094b407c358
nolan MARSHKE <nmarshke@gmail.com>
2025-03-10 15:35:27 -0400

    added type user

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -2,14 +2,22 @@ import { gql } from 'apollo-server-express';
 
 
 const typeDefs = gql`
-type Book {
-    bookId: String!
-    authors: [String]
-    description: String!
-    title: String!
-    image: String
-    link: String
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String
 }
 
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
 
 
