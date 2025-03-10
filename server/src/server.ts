fb5a0dcba0bc520b2e0c480e83441e25e232bdd8
nolan MARSHKE <nmarshke@gmail.com>
2025-03-10 16:02:06 -0400

    added client side queries

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,53 @@
 import express from 'express';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
-import routes from './routes/index.js';
+import { authMiddleware } from './services/auth.js';
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
 }
 
-app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  
+ 
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+};
+
+
+startApolloServer();
