60d930b29c7db1d4b645e72dca9e200ab9b2f164
nolan MARSHKE <nmarshke@gmail.com>
2025-03-10 15:49:30 -0400

    finished the resolvers

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -14,4 +14,54 @@ const resolvers = {
     },
 
 
+    Mutation: {
     
+        login: async (_parent: any, { email, password }: { email: string; password: string }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError('No user found with this email address');
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+    
+          const token = signToken(user.username, user.email, user._id);
+    
+          return { token, user };
+        },
+    
+        addUser: async (_parent: any, { username, email, password }: { username: string; email: string; password: string }) => {
+          const user = await User.create({ username, email, password });
+          const token = signToken(user.username, user.email, user._id);
+          return { token, user };
+        },
+    
+        saveBook: async (_parent: any, { input }: { input: any }, context: any) => {
+          if (context.user) {
+            return User.findOneAndUpdate(
+              { _id: context.user._id },
+              { $addToSet: { savedBooks: input } },
+              { new: true, runValidators: true }
+            );
+          }
+          throw new AuthenticationError('You need to be logged in!');
+        },
+    
+        removeBook: async (_parent: any, { bookId }: { bookId: string }, context: any) => {
+          if (context.user) {
+            return User.findOneAndUpdate(
+              { _id: context.user._id },
+              { $pull: { savedBooks: { bookId } } },
+              { new: true }
+            );
+          }
+          throw new AuthenticationError('You need to be logged in!');
+        },
+      },
+    };
+    
+    export default resolvers;
