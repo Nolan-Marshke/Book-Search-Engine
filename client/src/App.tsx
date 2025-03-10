6f0696fbffd9f7d3cfa971eed918427ceece0de2
nolan MARSHKE <nmarshke@gmail.com>
2025-03-10 16:53:44 -0400

    fixed more errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,17 @@ import { setContext } from '@apollo/client/link/context';
 
 import Navbar from './components/Navbar';
 
+
 const httpLink = createHttpLink({
-  uri: 'graphql',
+  uri: '/graphql',
 });
 
 
 const authLink = setContext((_, { headers }) => {
+  
   const token = localStorage.getItem('id_token');
+  
+  
   return {
     headers: {
       ...headers,
@@ -20,12 +24,11 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
-
-
 
 function App() {
   return (
